ac32ce39ac43be32eff47c295ad4967a2cf4c3ad
juanitodread <juanitodread@gmail.com>
2012-03-14 23:31:48 -0700

    Kata 1 - coding dojo - java gdl

---
+++ coding-dojo/src/org/juanitodread/javagdl/codingdojo/kata1/Main.java
@@ -0,0 +1,13 @@
+package org.juanitodread.javagdl.codingdojo.kata1;
+
+/**
+ * User: Jvan
+ * Date: 3/14/12
+ * Time: 11:25 PM
+ */
+public class Main {
+    public static void main(String[] args) {
+        int natural = 19;
+        System.out.printf("[Natural:%d, Romano:%s]%n", natural, ParserRomanNumbers.parserNaturalToRoman(natural));
+    }
+}
---
+++ coding-dojo/src/org/juanitodread/javagdl/codingdojo/kata1/ParserRomanNumbers.java
@@ -0,0 +1,33 @@
+package org.juanitodread.javagdl.codingdojo.kata1;
+
+/**
+ * User: juanitodread
+ * Date: 3/14/12
+ * Time: 11:24 PM
+ */
+public class ParserRomanNumbers {
+    public static int[] arabigos = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    public static String[] romanos = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+
+
+    public static String parserNaturalToRoman(int natural){
+        if(natural > 3999 || natural < 1){
+            return "Número fuera de rango :(";
+        }
+        String romano = "";
+        int denominador, residuo,   repetir;
+        for(int base = 0; base < arabigos.length; base++){
+            if(natural >= arabigos[base]){
+                residuo = natural % arabigos[base];
+                denominador = natural - residuo;
+                repetir = denominador / arabigos[base];
+                for(int i = 0; i < repetir; i++){
+                    romano = romano.format("%s%s", romano, romanos[base]);
+                }
+                natural = residuo;
+            }
+        }
+
+        return romano;
+    }
+}
